372247b2674bed7d00b314c73f15a8ec0baefd73
Ken Yasue <ken.yasue@clover-studio.com>
2017-08-01 17:22:39 +0200

    add webhook parser

--- init.js
+++ init.js
@@ -1,3 +1,4 @@
 module["exports"] = {
-    facebookVerifyToken: 'chinchin'
+    facebookVerifyToken: 'chinchin',
+    facebookAccessTOken: 'EAAB388h4QXwBAIcubZA9qAtRWH0MrBUowkfUdfxisI9P1B205dgMROOmqrqyXEg97PiBMuoClyhMJEadqniAZAAIQcoxOOSXS91lrIeYlq6ZCdsy3bZB9wJ3p8KKoOtG2fthFmaDPCNZC6k6ZCrbjqD0gEEva0AZCedi6PbO58ZBsAZDZD'
 };
