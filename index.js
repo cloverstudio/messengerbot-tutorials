988a5510e4e5a50d1a403ef10adb8c5f060602e8
Ken Yasue <ken.yasue@clover-studio.com>
2017-08-01 16:38:03 +0200

    hello world

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('test');
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 3000!')
+})
