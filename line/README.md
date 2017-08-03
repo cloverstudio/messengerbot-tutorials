91ed3b5b0906e08d41379b38fd441445f3537bcd
Ken Yasue <ken.yasue@clover-studio.com>
2017-08-03 13:38:46 +0200

    no message

--- line/README.md
+++ line/README.md
@@ -18,3 +18,21 @@
         - Select Accounts ( Btw, You can create new bot here )
         - Click "Line Developers"
         - Here you can setup webhook URL
+
+    - Verifing webhook signature is little bit confused.
+        - First you have to get raw request body, [this article](https://coderwall.com/p/qrjfcw/capture-raw-post-body-in-express-js) was use ful.
+
+        - Then user this source code to do verification.
+
+        ```
+        const signatureOrig = req.headers['x-line-signature'];
+        
+        // validate signature
+        const channelSecret = init.lineChannelSecret;
+
+        const body = req.rawBody; // Request body string
+        const signatureGenerated = crypto.createHmac('SHA256', channelSecret).update(body).digest('base64');
+        // Compare X-Line-Signature request header and the signature
+
+        console.log(signatureOrig,signatureGenerated);
+        ```
--- line/webhook.js
+++ line/webhook.js
@@ -1,16 +1,17 @@
-var express = require('express');
-var request = require('request');
-var router = express.Router();
-var crypto = require('crypto');
+const express = require('express');
+const request = require('request');
+const router = express.Router();
+const crypto = require('crypto');
+const line = require('@line/bot-sdk');
 
-var init = require('../init.js');
+const init = require('../init.js');
 
 var WebHookController = function(){
 }
 
 WebHookController.prototype.init = function(app){
 
-    var self = this;
+    const self = this;
 
     router.get('/webhook', function(req, res) {
         res.send('OK');
@@ -18,7 +19,8 @@ WebHookController.prototype.init = function(app){
 
     router.post('/webhook',(req,res) => {
 
-        var data = req.body;
+        const self = this;
+        const data = req.body;
 
         console.log("received line webhook",JSON.stringify(data, null, 3));
         console.log("headers",JSON.stringify(req.headers, null, 3));
@@ -34,13 +36,47 @@ WebHookController.prototype.init = function(app){
 
         console.log(signatureOrig,signatureGenerated);
 
-        res.send('OK');
+        if(signatureGenerated == signatureOrig){
+
+            if(data.events && data.events.length > 0
+                && data.events[0].replyToken)
+                // valid line request
+                this.replyToMessage(data.events[0].replyToken);
+            else
+                res.send('OK');
+        } else {
+            res.send('OK');
+        }
+
+        
 
     });
 
     return router;
 
 };
-;
+
+WebHookController.prototype.replyToMessage = function(replyToken){
+
+    const client = new line.Client({
+        channelAccessToken: init.lineChannelSecret
+    });
+
+    const message = {
+        type: 'text',
+        text: 'ちんちん'
+    };
+
+    client.replyMessage(replyToken, message)
+
+        .then(() => {
+            console.log('success to send message');
+        })
+        .catch((err) => {
+            console.log('failed to reply message',err)
+        });
+            
+    };
+
 
 module["exports"] = WebHookController;
