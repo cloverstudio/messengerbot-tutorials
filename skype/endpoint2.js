736af88f0df8e928d8a2146015acfc625b4d287e
Ken Yasue <ken.yasue@clover-studio.com>
2017-08-04 17:20:24 +0200

    no message

---
+++ skype/endpoint2.js
@@ -0,0 +1,62 @@
+var builder = require('botbuilder');
+
+const init = require('../init.js');
+
+var SkypeendpointController = function(){
+}
+
+SkypeendpointController.prototype.init = function(app){
+
+    var connector = new builder.ChatConnector({
+        appId: "3aea336b-b779-4658-a308-8f4b2297bf27",
+        appPassword: "VjAqNXDmwOQsmkhLyDg3b6g"
+    });
+
+    var bot = new builder.UniversalBot(connector);
+
+    //Bot on
+    bot.on('contactRelationUpdate', function (message) {
+
+        console.log('contactRelationUpdate',message);
+
+        if (message.action === 'add') {
+            var name = message.user ? message.user.name : null;
+            var reply = new builder.Message()
+                    .address(message.address)
+                    .text("Hello %s... Thanks for adding me. Say 'hello' to see some great demos.", name || 'there');
+            bot.send(reply);
+        } else {
+            // delete their data
+        }
+    });
+    bot.on('typing', function (message) {
+        // User is typing
+        console.log('typing',message);
+    });
+
+    bot.on('deleteUserData', function (message) {
+        // User asked to delete their data
+        console.log('deleteUserData',message);
+    });
+
+    bot.on('conversationUpdate', function (message) {
+        console.log('conversationUpdate',message);
+    });
+
+    //=========================================================
+    // Bots Dialogs
+    //=========================================================
+    String.prototype.contains = function(content){
+        return this.indexOf(content) !== -1;
+    }
+
+    bot.dialog('/', function (session) {
+        session.send("ともだちんこ");
+    });
+
+    return connector.listen();
+    
+};
+
+
+module["exports"] = SkypeendpointController;
